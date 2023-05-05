52cbfafd72f1aa8053c589914bd3e66dd4819124
Siddhi GUPTA <114733804+Siddhi04g@users.noreply.github.com>
2023-05-05 11:55:34 +0530

    Time: 13 ms (90.23%), Space: 43.2 MB (41.85%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,14 +1,5 @@
 class Solution {
-    // private int count(List<Character> l){
-    //     int c = 0;
-    //     for(char val : l){
-    //         if(val == 'a'|| val == 'e' || val == 'i' || val == 'o' || val == 'u')
-    //             c ++;
-    //     }
-    //     return c;
-    // }
     public int maxVowels(String s, int k) {
-        //List<Character> list = new ArrayList<>();
         int numvow = 0;
         for(int i = 0 ; i < k ; i++){
             char val = s.charAt(i);
@@ -23,12 +14,9 @@ class Solution {
             char rem = s.charAt(i);
             if(rem == 'a'|| rem == 'e' || rem == 'i' || rem == 'o' || rem == 'u')
                 numvow --;
-            //list.remove(0);
-            //System.out.print(i+" ");
             char val = s.charAt(j);
             if(val == 'a'|| val == 'e' || val == 'i' || val == 'o' || val == 'u')
                 numvow ++;
-            //list.add(val);
             max = Math.max(max , numvow);
             i++;
             j++;
