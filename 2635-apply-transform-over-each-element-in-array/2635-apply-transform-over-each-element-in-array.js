8cf6e38f4ff6240a856aeb01c159dea6d04fa4e3
Siddhi GUPTA <114733804+Siddhi04g@users.noreply.github.com>
2023-05-08 09:22:41 +0530

    Time: 58 ms (57.62%), Space: 42.1 MB (47.49%) - LeetHub

---
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    let newarr = [];
+    for( let i = 0 ; i < arr.length ; i++){
+        newarr[i] = fn(arr[i], i);
+    }
+    return newarr;
+};
