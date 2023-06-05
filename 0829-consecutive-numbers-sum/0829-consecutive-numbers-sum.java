4491e2edc1ea40b360e4f6b3911cd0e81a437315
Siddhi GUPTA <114733804+Siddhi04g@users.noreply.github.com>
2023-06-05 19:17:38 +0530

    Time: 5 ms (61.69%), Space: 39.2 MB (94.53%) - LeetHub

--- 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.java
+++ 0829-consecutive-numbers-sum/0829-consecutive-numbers-sum.java
@@ -7,3 +7,18 @@ class Solution {
         return count;
     }
 }
+
+/*
+
+1 + 2 + 3 + 4 + 5 = 15
+a + (a+1) + (a+2) + (a+3) + (a+4) = 15
+(a+a+a+a+a) + (1+2+3+4+5) = 15
+a*n + n(n-1) / 2 = sum
+
+now,
+a = (sum - n*(n-1)/2)/n
+
+for a to be greater than zero (sum - n*(n-1)/2) > 0
+and for a to be integer as we need only sum of integers (sum - n*(n-1)/2)%n == 0
+
+*/
